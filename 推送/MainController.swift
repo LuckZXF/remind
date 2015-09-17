819c1e1f512dbb025cf89526ef573dc1fb561f5b
赵希帆 <179988305@qq.com>
2015-09-17 16:39:54 +0800

    first step

---
+++ 推送/MainController.swift
@@ -0,0 +1,13 @@
+//
+//  MainController.swift
+//  推送
+//
+//  Created by 赵希帆 on 15/9/16.
+//  Copyright (c) 2015年 赵希帆. All rights reserved.
+//
+
+import Cocoa
+
+class MainController: UITableViewController {
+
+}
---
+++ 推送/MainListController.swift
@@ -0,0 +1,13 @@
+//
+//  MainListController.swift
+//  推送
+//
+//  Created by 赵希帆 on 15/9/16.
+//  Copyright (c) 2015年 赵希帆. All rights reserved.
+//
+
+import Cocoa
+
+class MainListController: UITableViewController {
+
+}
