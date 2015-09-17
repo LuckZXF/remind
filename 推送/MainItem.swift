d60bc44dfd4e444cca7f13aa1e680cab5c735785
赵希帆 <179988305@qq.com>
2015-09-17 17:25:05 +0800

    init

--- 推送/MainItem.swift
+++ 推送/MainItem.swift
@@ -6,8 +6,31 @@
 //  Copyright (c) 2015年 赵希帆. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class MainItem: NSObject {
-
+    
+    var text :String
+    
+    var checked : Bool
+    
+    init(text : String , checked : Bool){
+        self.text = text
+        self.checked = checked
+    }
+    
+    func toggleChecked(){
+        self.checked = !self.checked
+    }
+    
+    init!(coder aDecoder: NSCoder!) {
+        self.text = aDecoder.decodeObjectForKey("Text") as! String
+        self.checked = aDecoder.decodeObjectForKey("Checked") as! Bool
+        
+    }
+    
+    func encodeWithCoder(aCoder : NSCoder!) {
+        aCoder.encodeObject(text, forKey: "Text")
+        aCoder.encodeObject(checked, forKey: "Checked")
+    }
 }
